083218905f4c5b1e6f077503ed31012c992f1159
Brad <bkmorse@gmail.com>
2013-02-26 15:11:27 -0500

    changed db username

--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ return array(
 		'mysql' => array(
 			'driver'   => 'mysql',
 			'host'     => '127.0.0.1',
-			'database' => 'howdy',
+			'database' => 'dowdy',
 			'username' => 'ticket_user',
 			'password' => '12301980',
 			'charset'  => 'utf8',
